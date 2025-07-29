d24d6642558db8e935cfee604e5672ded56dd477
Danyil <shelofast.daniil@gmail.com>
2025-07-29 15:07:57 +0300

    corrections class MachineService

--- src/main/java/core/mate/academy/service/MachineService.java
+++ src/main/java/core/mate/academy/service/MachineService.java
@@ -1,9 +1,10 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Machine;
+
 import java.util.List;
 
-public interface MachineService<T> {
+public interface MachineService<T extends Machine> {
 
     List<T> getAll(Class<? extends T> type);
 
